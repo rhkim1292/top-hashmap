6ad1e17070f3d4a851fd44271a9c589621384bb6
Randy H Kim <rhkim1292@gmail.com>
2024-01-25 13:54:09 -0800

    Implement the set method in the HashMap factory
    A few variables have been added to the HashMap factory function
    to make the set method work.

--- HashMap.js
+++ HashMap.js
@@ -1,7 +1,11 @@
 const HashMap = () => {
+	const loadFactor = 0.75;
+	let capacity = 16;
+	const buckets = [];
+
 	const hash = (str) => {
-		if (typeof str !== 'string')
-			throw new Error('Must pass data of type string into hash method!');
+		if (typeof str !== "string")
+			throw new Error("Must pass data of type string into hash method!");
 
 		let hashCode = 0;
 
@@ -13,10 +17,20 @@ const HashMap = () => {
 		return hashCode;
 	};
 
-	const set = (key, value) => {};
+	const set = (key, value) => {
+		const index = hash(key) % capacity;
+
+		buckets[index] = [key, value];
+
+		if (buckets.length / capacity > loadFactor) capacity = capacity * 2;
+	};
 
 	return {
 		hash,
 		set,
 	};
 };
+
+const hashmap = HashMap();
+console.log(hashmap.hash("Sara"));
+console.log(hashmap.hash("raSa"));
